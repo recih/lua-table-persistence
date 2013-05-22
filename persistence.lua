034bf6912e70ffaf447e168267c8607aec8d543d
recih <fengbocih+github@gmail.com>
2013-05-22 16:15:26 +0800

    remove semicolons

--- persistence.lua
+++ persistence.lua
@@ -11,94 +11,94 @@
 --[[ License: MIT (see bottom) ]]
 
 -- Private methods
-local write, writeIndent, writers, refCount;
+local write, writeIndent, writers, refCount
 
 persistence =
 {
 	store = function (path, ...)
-		local file, e;
+		local file, e
 		if type(path) == "string" then
 			-- Path, open a file
-			file, e = io.open(path, "w");
+			file, e = io.open(path, "w")
 			if not file then
-				return error(e);
+				return error(e)
 			end
 		else
 			-- Just treat it as file
-			file = path;
+			file = path
 		end
-		local n = select("#", ...);
+		local n = select("#", ...)
 		-- Count references
-		local objRefCount = {}; -- Stores reference that will be exported
+		local objRefCount = {} -- Stores reference that will be exported
 		for i = 1, n do
-			refCount(objRefCount, (select(i,...)));
-		end;
+			refCount(objRefCount, (select(i,...)))
+		end
 		-- Export Objects with more than one ref and assign name
 		-- First, create empty tables for each
-		local objRefNames = {};
-		local objRefIdx = 0;
-		file:write("-- Persistent Data\n");
-		file:write("local multiRefObjects = {\n");
+		local objRefNames = {}
+		local objRefIdx = 0
+		file:write("-- Persistent Data\n")
+		file:write("local multiRefObjects = {\n")
 		for obj, count in pairs(objRefCount) do
 			if count > 1 then
-				objRefIdx = objRefIdx + 1;
-				objRefNames[obj] = objRefIdx;
-				file:write("{};"); -- table objRefIdx
-			end;
-		end;
-		file:write("\n} -- multiRefObjects\n");
+				objRefIdx = objRefIdx + 1
+				objRefNames[obj] = objRefIdx
+				file:write("{};") -- table objRefIdx
+			end
+		end
+		file:write("\n} -- multiRefObjects\n")
 		-- Then fill them (this requires all empty multiRefObjects to exist)
 		for obj, idx in pairs(objRefNames) do
 			for k, v in pairs(obj) do
-				file:write("multiRefObjects["..idx.."][");
-				write(file, k, 0, objRefNames);
-				file:write("] = ");
-				write(file, v, 0, objRefNames);
-				file:write(";\n");
-			end;
-		end;
+				file:write("multiRefObjects["..idx.."][")
+				write(file, k, 0, objRefNames)
+				file:write("] = ")
+				write(file, v, 0, objRefNames)
+				file:write(";\n")
+			end
+		end
 		-- Create the remaining objects
 		for i = 1, n do
-			file:write("local ".."obj"..i.." = ");
-			write(file, (select(i,...)), 0, objRefNames);
-			file:write("\n");
+			file:write("local ".."obj"..i.." = ")
+			write(file, (select(i,...)), 0, objRefNames)
+			file:write("\n")
 		end
 		-- Return them
 		if n > 0 then
-			file:write("return obj1");
+			file:write("return obj1")
 			for i = 2, n do
-				file:write(" ,obj"..i);
-			end;
-			file:write("\n");
+				file:write(" ,obj"..i)
+			end
+			file:write("\n")
 		else
-			file:write("return\n");
-		end;
-		file:close();
-	end;
+			file:write("return\n")
+		end
+		file:close()
+	end,
 
 	load = function (path)
-		local f, e = loadfile(path);
+		local f, e = loadfile(path)
 		if f then
-			return f();
+			return f()
 		else
-			return nil, e;
-		end;
-	end;
+			return nil, e
+		end
+	end
 }
 
 -- Private methods
 
 -- write thing (dispatcher)
 write = function (file, item, level, objRefNames)
-	writers[type(item)](file, item, level, objRefNames);
-end;
+	writers[type(item)](file, item, level, objRefNames)
+end
 
 -- write indent
 writeIndent = function (file, level)
 	for i = 1, level do
-		file:write("\t");
-	end;
-end;
+		file:write("\t")
+	end
+end
 
 -- recursively count references
 refCount = function (objRefCount, item)
@@ -106,79 +106,79 @@ refCount = function (objRefCount, item)
 	if type(item) == "table" then
 		-- Increase ref count
 		if objRefCount[item] then
-			objRefCount[item] = objRefCount[item] + 1;
+			objRefCount[item] = objRefCount[item] + 1
 		else
-			objRefCount[item] = 1;
+			objRefCount[item] = 1
 			-- If first encounter, traverse
 			for k, v in pairs(item) do
-				refCount(objRefCount, k);
-				refCount(objRefCount, v);
-			end;
-		end;
-	end;
-end;
+				refCount(objRefCount, k)
+				refCount(objRefCount, v)
+			end
+		end
+	end
+end
 
 -- Format items for the purpose of restoring
 writers = {
 	["nil"] = function (file, item)
-			file:write("nil");
-		end;
+			file:write("nil")
+		end,
 	["number"] = function (file, item)
-			file:write(tostring(item));
-		end;
+			file:write(tostring(item))
+		end,
 	["string"] = function (file, item)
-			file:write(string.format("%q", item));
-		end;
+			file:write(string.format("%q", item))
+		end,
 	["boolean"] = function (file, item)
 			if item then
-				file:write("true");
+				file:write("true")
 			else
-				file:write("false");
+				file:write("false")
 			end
-		end;
+		end,
 	["table"] = function (file, item, level, objRefNames)
-			local refIdx = objRefNames[item];
+			local refIdx = objRefNames[item]
 			if refIdx then
 				-- Table with multiple references
-				file:write("multiRefObjects["..refIdx.."]");
+				file:write("multiRefObjects["..refIdx.."]")
 			else
 				-- Single use table
-				file:write("{\n");
+				file:write("{\n")
 				for k, v in pairs(item) do
-					writeIndent(file, level+1);
-					file:write("[");
-					write(file, k, level+1, objRefNames);
-					file:write("] = ");
-					write(file, v, level+1, objRefNames);
-					file:write(";\n");
+					writeIndent(file, level+1)
+					file:write("[")
+					write(file, k, level+1, objRefNames)
+					file:write("] = ")
+					write(file, v, level+1, objRefNames)
+					file:write(";\n")
 				end
-				writeIndent(file, level);
-				file:write("}");
-			end;
-		end;
+				writeIndent(file, level)
+				file:write("}")
+			end
+		end,
 	["function"] = function (file, item)
 			-- Does only work for "normal" functions, not those
 			-- with upvalues or c functions
-			local dInfo = debug.getinfo(item, "uS");
+			local dInfo = debug.getinfo(item, "uS")
 			if dInfo.nups > 0 then
-				file:write("nil --[[functions with upvalue not supported]]");
+				file:write("nil --[[functions with upvalue not supported]]")
 			elseif dInfo.what ~= "Lua" then
-				file:write("nil --[[non-lua function not supported]]");
+				file:write("nil --[[non-lua function not supported]]")
 			else
-				local r, s = pcall(string.dump,item);
+				local r, s = pcall(string.dump,item)
 				if r then
-					file:write(string.format("loadstring(%q)", s));
+					file:write(string.format("loadstring(%q)", s))
 				else
-					file:write("nil --[[function could not be dumped]]");
+					file:write("nil --[[function could not be dumped]]")
 				end
 			end
-		end;
+		end,
 	["thread"] = function (file, item)
-			file:write("nil --[[thread]]\n");
-		end;
+			file:write("nil --[[thread]]\n")
+		end,
 	["userdata"] = function (file, item)
-			file:write("nil --[[userdata]]\n");
-		end;
+			file:write("nil --[[userdata]]\n")
+		end,
 }
 
 return persistence
